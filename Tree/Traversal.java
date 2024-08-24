cd21ae7a7d4f8eca874c2b35754aff8b140d3149
Shobhit Singh <shobhit2k21@gmail.com>
2024-08-24 16:48:21 +0530

    Created Tree

---
+++ Tree/Traversal.java
@@ -0,0 +1,14 @@
+package Tree;
+public class Traversal{
+    public static void main(String[] args) {
+        System.out.println("Hello from tree");
+    }
+
+
+    void preOrder(TreeNode node){
+
+    }
+    void postOrder(TreeNode root){
+
+    }
+}
---
+++ Tree/TreeNode.java
@@ -0,0 +1,9 @@
+package Tree;
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+    public TreeNode(int val){
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
